957dce2a6d8882fc2d0bbfd4e689e836ed425c01
Jason Lima Reyes <ladiesman6974@gmail.com>
2019-01-03 11:36:53 -0500

    Teets

---
+++ TweetMVVM/TweetMVVM/ViewModels/PostsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using TweetMVVM.Models;
+
+namespace TweetMVVM.ViewModels
+{
+    public class PostsViewModel
+    {
+        public System.Collections.ObjectModel.ObservableCollection<Post> Posts { get; set; }
+
+        public PostsViewModel ()
+        {
+            Posts = new ObservableCollection<Post>();
+
+            Posts.Add(new Post { Message = "Bengay", Username = "Bertrum" });
+            Posts.Add(new Post { Message = "E", Username = "Bean" });
+            Posts.Add(new Post { Message = "AAAAA", Username = "Thot" });
+            Posts.Add(new Post { Message = "You<Thanos", Username = "Thanos" });
+            Posts.Add(new Post { Message = "I", Username = "T" });
+
+
+        }
+    }
+}
